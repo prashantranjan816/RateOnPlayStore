042dde03430bf33847e40683268e567dcd075a88
prashantranjan <prashantranjan816@gmail.com>
2019-04-21 16:52:59 +0530

    Initial commit

---
+++ app/src/main/java/com/example/rateonplaystore/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.rateonplaystore;
+
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void rateMe(View view) {
+
+        try {
+            startActivity(new Intent(Intent.ACTION_VIEW,
+                    Uri.parse("market://details?id=" + "com.zipgo.customer"))); // Mention your app package name or just getPackageName()
+        } catch (ActivityNotFoundException e) {
+            startActivity(new Intent(Intent.ACTION_VIEW,
+                    Uri.parse("http://play.google.com/store/apps/details?id=" + getPackageName())));
+        }
+    }
+}
